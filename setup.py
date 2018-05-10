ef23cd58b8684bcd41e1b579cfdc38c8794dc10e
mwas <francismwangi152@gmail.com>
2018-05-10 13:10:46 +0300

    upgrading version.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import dynamodb_sessions
 
 long_description = open('README.rst').read()
 
-major_ver, minor_ver = dynamodb_sessions.__version__
-version_str = '%d.%d' % (major_ver, minor_ver)
+major_ver, minor_ver, minor_minor_ver = dynamodb_sessions.__version__
+version_str = '%d.%d.%d' % (major_ver, minor_ver, minor_minor_ver)
 
 setup(
     name='django-dynamodb-sessions',
